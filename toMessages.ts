0c0cef546236f7b7b5a813ce798d008029e07703
Alasdair Stores <alasdairwstores@gmail.com>
2024-07-31 09:29:12 +0000

    Update toMessages.ts

--- toMessages.ts
+++ toMessages.ts
@@ -1,14 +1,21 @@
 import { z } from "zod";
 
-export function toMessages<T>(parsed: z.SafeParseReturnType<any, T>) {
-  const messages: Record<string, string> = {};
+type FieldPath<T, Prefix extends string = ""> = {
+  [K in keyof T]: T[K] extends object
+    ? `${Prefix}${K & string}.${FieldPath<T[K]>}`
+    : `${Prefix}${K & string}`;
+}[keyof T] extends infer D
+  ? Extract<D, string>
+  : never;
+
+export function toMessages<T>(parsed: z.SafeParseReturnType<T, any>) {
+  const messages: Record<FieldPath<T>, string> = {};
   if (parsed.success) return messages;
 
   parsed.error.issues.forEach((issue) => {
-    const path = issue.path.join(".");
+    const path = issue.path.join(".") as FieldPath<T>;
     messages[path] = issue.message;
   });
 
   return messages;
 }
-
